8897883c25b3592a3396f239db62271d5056e2c5
waltersmatthew <waltersmatthew88@gmail.com>
2022-08-25 09:58:00 -0700

    dry out code, add one more building

--- main.js
+++ main.js
@@ -176,7 +176,8 @@ let fritz = new Fritz(25, 250, 65, 25, currentSprite, "image");
 let building1 = new Platform(0, 305, 250, canvas.height, fill, border);
 let building2 = new Platform(520, 130, 50, 65, "#E78DA2", "#FCBEA3");
 let building3 = new Platform(744, 320, 89, canvas.height, fill, border);
-let building4 = new Platform(1400, 180, 500, canvas.height, fill, "#617659");
+let building4 = new Platform(1350, 110, 50, 65, "#E78DA2", "#FCBEA3");
+let building5 = new Platform(1550, 180, 350, canvas.height, fill, "#617659");
 let plat1 = new Platform(325, 240, 40, 10, back, fill);
 let plat2 = new Platform(450, 200, 25, 10, "#FCBEA3", "#E88D9D");
 let plat3 = new Platform(680, 240, 25, 10, back, fill);
@@ -190,6 +191,7 @@ platforms.push(building1)
 platforms.push(building2)
 platforms.push(building3)
 platforms.push(building4)
+platforms.push(building5)
 platforms.push(plat1)
 platforms.push(plat2)
 platforms.push(plat3)
@@ -200,152 +202,14 @@ platforms.push(house)
 
 
 //init function to reset entire game
-function init(){
-    class Fritz {
-        constructor(x, y, width, height, color, type) {
-            this.x = x;
-            this.y = y;
-            this.width = width;
-            this.height = height;
-            this.color = color;
-            this.type = type;
-            if (type == "image") {
-                this.image = new Image();
-                this.image.src = color;
-            }
-            this.alive = true;
-            this.angle = 0;
-            this.velocity = {
-                x: 0,
-                y: 0,
-            };
-        }
-        update() {
-            this.x += this.velocity.x;
-            this.y += this.velocity.y;
-            this.velocity.y += gravity;
-            ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
-
-        }
-    }
-    //class for platforms and buildings
-    class Platform {
-        constructor(x, y, width, height, borderColor, fillColor) {
-            this.x = x;
-            this.y = y;
-            this.width = width;
-            this.height = height;
-            this.borderColor = borderColor;
-            this.fillColor = fillColor;
-        }
-        // makeBuilding(x, y, width, height)
-        makeBuilding() {
-            ctx.lineWidth = 5;
-            ctx.fillStyle = this.borderColor;
-            ctx.fillRect(this.x, this.y, this.width, this.height);
-            ctx.fillStyle = this.fillColor;
-            //windows
-            ctx.fillRect(this.x + 5, this.y + 5, this.width - 10, 10);
-            ctx.fillRect(this.x + 5, this.y + 25, this.width - 10, 10);
-            ctx.fillRect(this.x + 5, this.y + 45, this.width - 10, 10);
-        }
-        makePlat() {
-            ctx.strokeStyle = this.borderColor;
-            ctx.strokeRect(this.x, this.y, this.width, this.height);
-            ctx.fillStyle = this.fillColor;
-            ctx.fillRect(this.x, this.y, this.width, this.height);
-        }
-        makeHouse() {
-            //main square
-            ctx.lineWidth = 2;
-            ctx.fillStyle = "#FFFC40";
-            ctx.strokeStyle = "black";
-            ctx.strokeRect(this.x, this.y, this.width, this.height);
-            ctx.fillRect(this.x, this.y, this.width, this.height);
-            //roof
-            ctx.fillStyle = "#D0693E";
-            ctx.beginPath();
-            ctx.moveTo(this.x -10, this.y);
-            ctx.lineTo(this.x + 210, this.y);
-            ctx.lineTo(this.x + 165, 50);
-            ctx.lineTo(this.x + 45, 50);
-            ctx.lineTo(this.x -10, this.y);
-            ctx.fill();
-            //roof styling
-            ctx.beginPath();
-            ctx.moveTo(this.x + 25, this.y);
-            ctx.lineTo(this.x + 50, 50);
-            ctx.stroke();
-            ctx.beginPath();
-            ctx.moveTo(this.x + 40, this.y);
-            ctx.lineTo(this.x + 65, 50);
-            ctx.stroke();
-            ctx.beginPath();
-            ctx.moveTo(this.x + 55, this.y);
-            ctx.lineTo(this.x + 80, 50);
-            ctx.stroke();
-            ctx.beginPath();
-            ctx.moveTo(this.x + 70, this.y);
-            ctx.lineTo(this.x + 95, 50);
-            ctx.stroke();
-            ctx.beginPath();
-            ctx.moveTo(this.x + 85, this.y);
-            ctx.lineTo(this.x + 110, 50);
-            ctx.stroke();
-            ctx.beginPath();
-            ctx.moveTo(this.x + 100, this.y);
-            ctx.lineTo(this.x + 125, 50);
-            ctx.stroke();
-            ctx.beginPath();
-            ctx.moveTo(this.x + 115, this.y);
-            ctx.lineTo(this.x + 140, 50);
-            ctx.stroke();
-            ctx.beginPath();
-            ctx.moveTo(this.x + 130, this.y);
-            ctx.lineTo(this.x + 155, 50);
-            ctx.stroke();
-            ctx.beginPath();
-            ctx.moveTo(this.x + 145, this.y);
-            ctx.lineTo(this.x + 168, 55);
-            ctx.stroke();
-            //doors
-            ctx.fillStyle = "#E4DECB";
-            ctx.fillRect(this.x + 100, 130, 20, 50);
-            ctx.fillRect(this.x + 80, 130, 20, 50)
-            ctx.strokeStyle = "black";
-            ctx.strokeRect(this.x + 100, 130, 20, 50);
-            ctx.strokeRect(this.x + 80, 130, 20, 50)
-            //doorknob
-            ctx.beginPath();
-            ctx.arc(this.x + 95, 155, 2, 0, 2 * Math.PI);
-            ctx.stroke();
-            ctx.beginPath();
-            ctx.arc(this.x + 105, 155, 2, 0, 2 * Math.PI);
-            ctx.stroke();
-            //left window
-            ctx.fillStyle = "white"
-            ctx.fillRect(this.x + 15, this.y + 10, 40, 40)
-            ctx.strokeRect(this.x + 15, this.y + 10, 40, 40)
-            ctx.strokeRect(this.x + 15, this.y + 10, 20, 20)
-            ctx.strokeRect(this.x + 35, this.y + 30, 20, 20)
-            //right window
-            ctx.fillRect(this.x + this.width - 55, this.y + 10, 40, 40)
-            ctx.strokeRect(this.x + this.width - 55, this.y + 10, 40, 40)
-            ctx.strokeRect(this.x + this.width - 55, this.y + 10, 20, 20)
-            ctx.strokeRect(this.x + this.width - 35, this.y + 30, 20, 20)
-            //ball
-            ctx.fillStyle = "hotpink";
-            ctx.beginPath();
-            ctx.arc(this.x + 55, 172, 7, 0, 2 * Math.PI);
-            ctx.fill();
-        }
-    }
-    //create fritz and buildings
+function reset(){
+    //recreate fritz and buildings
     fritz = new Fritz(25, 250, 65, 25, currentSprite, "image");
     building1 = new Platform(0, 305, 250, canvas.height, fill, border);
     building2 = new Platform(520, 130, 50, 65, "#E78DA2", "#FCBEA3");
     building3 = new Platform(744, 320, 89, canvas.height, fill, border);
-    building4 = new Platform(1400, 180, 500, canvas.height, fill, "#617659");
+    building4 = new Platform(1350, 110, 50, 65, "#E78DA2", "#FCBEA3");
+    building5 = new Platform(1550, 180, 350, canvas.height, fill, "#617659");
     plat1 = new Platform(325, 240, 40, 10, back, fill);
     plat2 = new Platform(450, 200, 25, 10, "#FCBEA3", "#E88D9D");
     plat3 = new Platform(680, 240, 25, 10, back, fill);
@@ -359,6 +223,7 @@ function init(){
     platforms.push(building2)
     platforms.push(building3)
     platforms.push(building4)
+    platforms.push(building5)
     platforms.push(plat1)
     platforms.push(plat2)
     platforms.push(plat3)
@@ -367,7 +232,7 @@ function init(){
     platforms.push(plat6)
     platforms.push(house)
 }
-//click to play again
+//click window to play again
 document.addEventListener("click", () => {
     respawn();
     topText.innerText = "Get Fritz home to his favorite pink ball!";
@@ -376,14 +241,13 @@ document.addEventListener("click", () => {
 //respawn function
 function respawn() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    init()
+    reset()
     scrollOffset = 0
-    fritz = new Fritz(25, 250, 65, 25, "./img/8bitty.png", "image");
     for (let i = 0; i < platforms.length; i++) {
-        if(i < 4){
+        if(i < 5){
             platforms[i].makeBuilding()
         }
-        else if(i < 10){
+        else if(i < 11){
             platforms[i].makePlat()
         }else{
             platforms[i].makeHouse()
@@ -422,10 +286,10 @@ function gameLoop() {
 
     //render all platforms
     for (let i = 0; i < platforms.length; i++) {
-        if(i < 4){
+        if(i < 5){
             platforms[i].makeBuilding()
         }
-        else if(i < 10){
+        else if(i < 11){
             platforms[i].makePlat()
         }else{
             platforms[i].makeHouse()
@@ -505,7 +369,7 @@ function gameLoop() {
     //draw Fritz. MUST GO LAST!!
     fritz.update();
 }
-gameLoop();
+// gameLoop();
 
 //controller event listeners (key down and up)
 
